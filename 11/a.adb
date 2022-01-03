fe9d8a40f12155602f437156043c5b98d485a3c3
Lenni vH <lennivh24@gmail.com>
2022-01-03 09:55:57 +0100

    11: read file

--- 11/a.adb
+++ 11/a.adb
@@ -1,54 +1,141 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Exceptions; use Ada.Exceptions;
 
 procedure A is
 	type Seat is (E, O, F); -- Empty, Occupied or Floor
 	type Boat is ARRAY (Natural range <>, Natural range <>) of Seat;
 
-	X, Y : Integer;
+	X, Y : Natural;
 
-	Width : Natural := 6;
-	Height : Natural := 4;
-	MyBoat : Boat(0..6, 0..4);
+	Width : Natural := 10;
+	Height : Natural := 10;
+	MyBoat : Boat (1..Width, 1..Height);
 
-	procedure Clear_Boat is
+	Bad_Input : exception;
+
+	procedure Read_Boat (Name : String) is
+		File : File_Type;
 	begin
-		for X in 0..Width loop
-			for Y in 0..Height loop
-				MyBoat(X, Y) := E;
+		Open (File => File, Mode => In_File, Name => Name);
+
+		Y := 1;
+
+		declare
+			S : String := Get_Line (File);
+		begin
+			Reset (File);
+
+			while not End_Of_File (File) loop
+				S := Get_Line (File);
+				for X in 1..S'Length loop
+					case S(X) is
+						when 'L' => MyBoat(X, Y) := E;
+						when '#' => MyBoat(X, Y) := O;
+						when '.' => MyBoat(X, Y) := F;
+						when others => raise Bad_Input with "Invalid character in boat";
+					end case;
+				end loop;
+				Y := Y + 1;
 			end loop;
-		end loop;
-	end;
+		end;
+	end Read_Boat;
+
+	function Around return Natural is
+		Val : Natural := 0;
+	begin
+		Put ("Checking for");
+		Put (X'Image);
+		Put ("/");
+		Put (Y'Image);
+		Put (": ");
+		if Y > 1 then
+			if X > 1 and then MyBoat (X - 1, Y - 1) = O then
+				Val := Val + 1;
+			end if;
+			if MyBoat (X, Y - 1) = O then
+				Val := Val + 1;
+			end if;
+			if X <= Width and then MyBoat (X + 1, Y - 1) = O then
+				Val := Val + 1;
+			end if;
+		end if;
+
+		if X > 1 and then MyBoat (X - 1, Y) = O then
+			Val := Val + 1;
+		end if;
+		if X <= Width and then MyBoat (X + 1, Y) = O then
+			Val := Val + 1;
+		end if;
+
+		if Y <= Height then
+			if X > 1 and then MyBoat (X - 1, Y + 1) = O then
+				Val := Val + 1;
+			end if;
+			if MyBoat (X, Y + 1) =  O then
+				Val := Val + 1;
+			end if;
+			if X <= Width and then MyBoat (X + 1, Y + 1) = O then
+				Val := Val + 1;
+			end if;
+		end if;
+
+		Put_Line (Val'Image);
+
+		return Val;
+	end Around;
 
 	procedure Run_Once is
+		NextBoat : Boat (1..Width, 1..Height);
 	begin
-		Put_Line("hey");
+		for X in 1..Width loop
+			for Y in 1..Height loop
+				Put (X'Image);
+				Put (" -");
+				Put (Y'Image);
+				Put_Line (":");
+				if MyBoat (X, Y) = E and then Around = 0 then
+					NextBoat (X, Y) := O;
+				elsif MyBoat (X, Y) = O and then Around >= 4 then
+					NextBoat (X, Y) := E;
+				else
+					NextBoat (X, Y) := MyBoat (X, Y);
+				end if;
+			end loop;
+		end loop;
+		MyBoat := NextBoat;
 	end Run_Once;
-
-	procedure Boat_Value is
-	begin
-		Put_Line("hey");
-	end Boat_Value;
 
 	procedure Print_Boat is
 	begin
-		for Y in 0..Height loop
-			for X in 0..Width loop
-				Put(MyBoat(X, Y)'Image);
+		for Y in 1..Height loop
+			for X in 1..Width loop
+				--Put (MyBoat(X, Y)'Image);
+				case MyBoat (X, Y) is
+					when E => Put ("L");
+					when O => Put ("#");
+					when F => Put (".");
+				end case;
 			end loop;
-			Put_Line("");
+			Put_Line ("");
 		end loop;
 	end Print_Boat;
 
 begin
 	X := 0;
 	Y := 0;
+	
+	begin
+		Read_Boat ("test1.txt");
+	exception
+		when Bad_Input => Put("Cannot read input");
+	end;
 
-	Clear_Boat;
-	Put_Line("before:");
+	Put_Line ("before:");
 	Print_Boat;
 	Run_Once;
 
-	Put_Line("-----------");
-	Put_Line("afterwards:");
+	Put_Line ("-----------");
+	Put_Line ("afterwards:");
 	Print_Boat;
 end A;
